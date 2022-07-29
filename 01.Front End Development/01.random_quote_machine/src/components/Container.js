7df716a726c825700127b1fe03d6329eb4f11b4b
Alberto Del Angel <rockstar4400@gmail.com>
2022-07-29 09:32:45 -0500

    Remove Comments

--- 01.Front End Development/01.random_quote_machine/src/components/Container.js
+++ 01.Front End Development/01.random_quote_machine/src/components/Container.js
@@ -35,17 +35,7 @@ class Container extends React.Component {
   }
 
   sharingFacebook() {
-    // var fbButton = document.getElementById("fb-share-button");
     var url = window.location.href;
-
-    // fbButton.addEventListener("click", function () {
-    //   window.open(
-    //     "https://www.facebook.com/sharer/sharer.php?u=" + url,
-    //     "facebook-share-dialog",
-    //     "width=800,height=600", 
-    //   );
-    //   return false;
-    // });
 
     document.getElementById('fb-share-button').onclick = 
     function() {
